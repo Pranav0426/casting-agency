b93b6833baa28513cfd1d2af5a83f81ea0b6069f
Pranav0426 <alluguntipranav@karunya.edu.in>
2020-05-20 21:37:30 +0530

    cahnge callback url

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
 AUTH0_CLIENT_ID = 'eH9ApoEVliYbgzNZUV7fg5sNswpd7qqs'
 AUTH0_CLIENT_SECRET = 'rZOJ0xqCSp_ZkEcgUG3ZslpHPQXOvf7GcmUW2hKG8HUc0vWXWEWdwkk8nL5HQ_wq'
-AUTH0_CALLBACK_URL = 'http://localhost:8080/callback'
+AUTH0_CALLBACK_URL = 'https://casting-agency-0426.herokuapp.com/callback'
 AUTH0_DOMAIN = 'udacity-shop.auth0.com'
 AUTH0_AUDIENCE = 'agency'
