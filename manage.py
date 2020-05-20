7862b4bdf78703fcb6020fe19c111633bd589795
Pranav0426 <alluguntipranav@karunya.edu.in>
2020-05-20 21:06:38 +0530

    setup migrate and requirements

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from app import create_app
-from models import db
+from models import db, Actor, Movie
 
 app = create_app()
 
@@ -9,6 +9,16 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
+
+
+@manager.command
+def seed():
+    Movie(title='Terminator Dark Fate', release_date='2019-05-06').insert()
+    Movie(title='Terminator Rise of the machines',
+          release_date='2003-05-06').insert()
+
+    Actor(name='Will Smith', age=40, gender='male').insert()
+    Actor(name='Bruce Wills', age=50, gender='male').insert()
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -11,7 +11,6 @@ def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
-    db.create_all()
 
 
 class Actor(db.Model):
